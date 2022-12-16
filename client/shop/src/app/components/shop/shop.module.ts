6f0a48ae5ca69a4f5bfcb087d35eb8f1499be500
trozko <trozko@griddynamics.com>
2022-12-16 14:44:25 +0100

    dsc

--- client/shop/src/app/components/shop/shop.module.ts
+++ client/shop/src/app/components/shop/shop.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { ShopRoutingModule } from './shop-routing.module';
 import { ShopComponent } from './shop.component';
-<<<<<<< HEAD
 import { SharedModule } from 'src/app/shared/shared.module';
 import { FiltersComponent } from './filters/filters.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
@@ -13,17 +12,3 @@ import { ToolbarComponent } from './toolbar/toolbar.component';
   imports: [CommonModule, SharedModule, ShopRoutingModule],
 })
 export class ShopModule {}
-=======
-
-
-@NgModule({
-  declarations: [
-    ShopComponent
-  ],
-  imports: [
-    CommonModule,
-    ShopRoutingModule
-  ]
-})
-export class ShopModule { }
->>>>>>> 53ced05a7f046a5e08ab8741e64d2be24c61c3e7
