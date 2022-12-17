96321d88a38b64d585fcc587ea244f5e3d2792a5
Sebastian Mazur <102309928+Blejn@users.noreply.github.com>
2022-12-17 16:39:29 +0100

    card component

--- client/shop/src/app/app.module.ts
+++ client/shop/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AdministrationModule } from './components/administration/administration
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { PromotionBannerComponent } from './components/home-page/promotion-banner/promotion-banner.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProductService } from './services/product.service';
 import { UsersListService } from './services/users-list.service';
 import { SharedModule } from './shared/shared.module';
 
@@ -27,7 +28,7 @@ import { SharedModule } from './shared/shared.module';
     HttpClientModule,
     AdministrationModule,
   ],
-  providers: [UsersListService],
+  providers: [UsersListService, ProductService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
