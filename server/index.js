a4c5b224fba718412de424e602c37e92a885a89c
Sebastian Mazur <102309928+Blejn@users.noreply.github.com>
2022-12-19 17:27:04 +0100

    add promotion

--- server/index.js
+++ server/index.js
@@ -42,6 +42,35 @@ app.put("/users/edit/:id", jsonParser, async (req, res) => {
     res.status(500).json(err);
   }
 });
+app.post("/products/add", jsonParser, async (req, res) => {
+  try {
+    let randId = Math.floor(Math.random() * 10000000000);
+    const title = req.body.title;
+    const author = req.body.author;
+    const price = req.body.price;
+    const image = req.body.image;
+    const category = req.body.category;
+    const newProduct = await client.query(
+      "INSERT INTO products(id, title, author, price, image, category) VALUES ($1, $2, $3, $4, $5,$6)",
+      [randId, title, author, price, image, category]
+    );
+    res.status(200).json(newProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+app.get("/products/promotions", jsonParser, async (req, res) => {
+  try {
+    await client.query(
+      "SELECT * FROM products WHERE promotion = true",
+      (err, response) => {
+        res.status(200).json(response.rows);
+      }
+    );
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 app.listen(process.env.PORT || 3500, () => {
   console.log(`Server running`);
