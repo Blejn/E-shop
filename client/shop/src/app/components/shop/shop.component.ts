7e40810f060c7ba634c0268a3b748cbc610f55c0
trozko <trozko@griddynamics.com>
2022-12-19 20:25:21 +0100

    vscode merge dumb fix

--- client/shop/src/app/components/shop/shop.component.ts
+++ client/shop/src/app/components/shop/shop.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
 import { debounceTime, first } from 'rxjs';
-=======
-import { debounceTime } from 'rxjs';
->>>>>>> master
 import { FiltersService } from 'src/app/services/filters.service';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -13,7 +9,6 @@ import { ProductService } from 'src/app/services/product.service';
   styleUrls: ['./shop.component.scss'],
 })
 export class ShopComponent {
-<<<<<<< HEAD
   constructor(
     private filtersService: FiltersService,
     private productService: ProductService
@@ -21,9 +16,4 @@ export class ShopComponent {
 
   $filters = this.filtersService.$filters.pipe(debounceTime(600));
   $products = this.productService.getProducts().pipe(first());
-=======
-  constructor(private filtersService: FiltersService) {}
-
-  $filters = this.filtersService.$filters.pipe(debounceTime(600));
->>>>>>> master
 }
