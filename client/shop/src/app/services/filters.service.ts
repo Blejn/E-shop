5d0b6cb05ac45339c78a2bc63b34dcd1e802ac2c
trozko <trozko@griddynamics.com>
2022-12-19 20:21:36 +0100

    same

--- client/shop/src/app/services/filters.service.ts
+++ client/shop/src/app/services/filters.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Subject } from 'rxjs';
 import { Filters } from '../model/Filters';
+import { Product } from '../model/Product';
 
 @Injectable({
   providedIn: 'root',
@@ -15,4 +17,20 @@ export class FiltersService {
   get $filters() {
     return this.$filtersValues as Subject<Filters>;
   }
+
+  getFiltersOptions(products: Product[], filters: FormGroup<any>): typeof obj {
+    const controls = Object.keys(filters.controls);
+    const obj: { [key: string]: Set<string | number> } = {};
+
+    controls.forEach(
+      (control) =>
+        (obj[control] = new Set(
+          products
+            .map((product) => product[control as keyof Filters])
+            .filter((x) => x !== null || undefined)
+            .flat()
+        ))
+    );
+    return obj;
+  } // private metoda do pobierania mozliwych wartosci do selectow w filtrach
 }
